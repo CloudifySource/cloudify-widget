bb131dab1cbced52b76f96478729e6a6652b2044
Igor Goldenberg <igor@kenlo-group.com>
2012-11-27 10:03:13 +0200

    fixed misspelling

--- bin/deployer.sh
+++ bin/deployer.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 SERVER_IP=$1
-RECIPE_ULR=$2
+RECIPE_URL=$2
 RECIPE_TYPE=$3
 GS_HOME=~/gigaspaces-cloudify-2.2.0-ga
 
 cd $GS_HOME/bin
 
-./cloudify.sh "connect http://$SERVER_IP:8100;$RECIPE_TYPE $RECIPE_ULR" 
+./cloudify.sh "connect http://$SERVER_IP:8100;$RECIPE_TYPE $RECIPE_URL" 
 
 exit 1
