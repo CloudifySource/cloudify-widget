7ac62d710bad52980eabed75c527adbd73e62b14
Guy Mograbi <guym@gigaspaces.com>
2013-01-01 12:49:35 +0200

    adding a script to initialize the machine. currently the script assumes the following:  - we are initializing a CENTOS machine  - we are running at /root folder  - prod.conf and sysconfig/play files are available

---
+++ centos_auto_setup_machine.sh
@@ -0,0 +1,54 @@
+
+yum  -y install java-1.6.0-openjdk-devel
+export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk.x86_64
+
+wget 'http://download.playframework.org/releases/play-2.0.4.zip'
+unzip play-2.0.4.zip
+
+wget 'http://repository.cloudifysource.org/org/cloudifysource/2.2.0-RELEASE/gigaspaces-cloudify-2.2.0-ga-b2500.zip'
+unzip gigaspaces-cloudify-2.2.0-ga-b2500.zip
+
+
+
+yum  -y install git
+cd play-2.0.4
+git clone https://github.com/CloudifySource/cloudify-widget.git
+
+yum install -y ruby rubygems
+gem install sass
+
+# assuming sysconfig_play exists on machine
+cp ~/sysconfig_play /etc/sysconfig/play
+. /etc/sysconfig/play
+
+# assuming there is a prod.conf copied to here
+mv ~/prod.conf cloudify-widget/conf
+chmod 755 cloudify-widget/*.sh
+chmod 755 cloudify-widget/bin/*.sh
+
+#install mysql
+yum -y install mysql-server mysql php-mysql
+chkconfig --levels 235 mysqld on
+service mysqld start
+mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost' = PASSWORD('$DB_ADMIN_PASSWORD');"
+mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');"
+mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'127.0.0.1' = PASSWORD('$DB_ADMIN_PASSWORD');"
+mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');"
+mysql -u $DB_ADMIN -e "SET PASSWORD FOR '$DB_ADMIN'@'localhost.localdomain' = PASSWORD('$DB_ADMIN_PASSWORD');"
+mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD -e "DROP USER ''@'localhost';"
+mysql -u $DB_ADMIN -e "DROP USER ''@'localhost';"
+mysql -u $DB_ADMIN -p$DB_ADMIN_PASSWORD  -e  "DROP USER ''@'localhost.localdomain';"
+mysql -u $DB_ADMIN -e "DROP USER ''@'localhost.localdomain';"
+
+bin/migrate_db.sh create
+#find which is the latest version of DB
+# ll all the files, remove "create" script, remove extension, sort in descending order and output first line.
+db_version=`ls conf/evolutions/default -1 | grep -v create |  sed -e 's/\.[a-zA-Z]*$//' | sort -r | head -1`
+bin/migrate_db.sh $db_version
+
+echo "127.0.0.1 `hostname`" >> /etc/hosts
+
+
+
+
+
