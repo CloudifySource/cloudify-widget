dd0f02afe328ebe7639a711e7fdb7d52b6e2653b
sagioto <sagioto@gmail.com>
2013-01-06 20:10:32 +0200

    added test and scripts

--- test/src/main/scripts/test.sh
+++ test/src/main/scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # install maven
+cd /root
 wget http://apache.spd.co.il/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
 tar xvf apache-maven-3.0.4-bin.tar.gz 
 export M2_HOME=/root/apache-maven-3.0.4 
@@ -23,7 +24,9 @@ gem install hpcloud-1.4.0.gem
 cd /root/play-2.0.4/cloudify-widget
 ./play_run.sh &
 
-
+url=http://localhost:9000
+while ! wget --spider $url 2>&1 | grep --quiet "200 OK"; do sleep 1; echo waiting for site...; done;
+	
 #start the tests
 cd /root/play-2.0.4/cloudify-widget/test
 mvn test -U -X
