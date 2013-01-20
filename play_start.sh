ae091faa9c28e0f2c82921b2785a95304498f25b
Guy Mograbi <guym@gigaspaces.com>
2013-01-20 10:21:00 +0200

    separating "setup" from "upgrade" upgrade is for setup operations we want to do every time. the "setup" script calls the "upgrade" script.
    adding monit to the setup
    fixing the sysvinit scripts.
    adding draft for monit configuration files.

--- play_start.sh
+++ play_start.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env sh
 
 if [ -f /var/lib/takipi/so/libTakipiAgent.so ]; then
-    _JAVA_OPTIONS=" ${_JAVA_OPTIONS} -agentpath:/var/lib/takipi/so/libTakipiAgent.so"
+    echo "adding takipi to java options"
+    _JAVA_OPTIONS="${_JAVA_OPTIONS} -agentpath:/var/lib/takipi/so/libTakipiAgent.so"
 fi
 nohup play -Dconfig.file=conf/prod.conf start &
