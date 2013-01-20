28a7e63626b6eaefc2a1a5b0bf86e280100335fa
Guy Mograbi <guym@gigaspaces.com>
2013-01-20 09:31:35 +0200

    separating "setup" from "upgrade" upgrade is for setup operations we want to do every time. the "setup" script calls the "upgrade" script.
    adding monit to the setup
    fixing the sysvinit scripts.
    adding draft for monit configuration files.

--- upgrade_server.sh
+++ upgrade_server.sh
@@ -31,4 +31,7 @@ echo "upgrading init.d script"
 chmod 755 /etc/init.d/widget
 
 echo "upgrading monit configurations"
-cat conf/monit/widget.monit | sed 's/__monit_pidfile__/'"$MONIT_PIDFILE"'/' > /etc/monit.d/widget
+cat conf/monit/conf.monit | sed 's,__monit_from__,'"$MONIT_FROM"',' | sed 's,__monit_to__,'"$MONIT_SET_ALERT"',' > /etc/monit.conf
+mv conf/monit/mysql.monit /etc/monit.d/mysqld
+MONIT_PIDFILE=$WIDGET_HOME/RUNNING_PID
+cat conf/monit/widget.monit | sed 's,__monit_pidfile__,'"$MONIT_PIDFILE"',' > /etc/monit.d/widget
