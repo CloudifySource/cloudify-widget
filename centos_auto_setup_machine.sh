cc61a75023a26f6bbe9cf371cc2b7a378612a06b
Guy Mograbi <guym@gigaspaces.com>
2013-01-20 09:31:07 +0200

    separating "setup" from "upgrade" upgrade is for setup operations we want to do every time. the "setup" script calls the "upgrade" script.
    adding monit to the setup
    fixing the sysvinit scripts.
    adding draft for monit configuration files.

--- centos_auto_setup_machine.sh
+++ centos_auto_setup_machine.sh
@@ -68,10 +68,7 @@ echo "intalling monit"
 \cp -Rf conf/monit/repo  /etc/yum.repos.d/epel.repo
 yum -y install monit
 chkconfig --levels 235 monit on
-cat conf/monit/conf.monit | sed 's/__monit_from__/'"$MONIT_FROM"'/' | sed 's/__monit_to__/'"$MONIT_SET_ALERT"'/' > /etc/monit.conf
-mv conf/monit/mysql.monit /etc/monit.d/mysqld
-MONIT_PIDFILE=$WIDGET_HOME/RUNNING_PID
-cat conf/monit/widget.monit | sed 's/__monit_pidfile__/'"$MONIT_PIDFILE"'/' > /etc/monit.d/widget
+
 
 echo "upgrading system"
 upgrade_server
