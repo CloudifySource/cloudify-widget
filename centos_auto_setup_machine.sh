53c54523e10e6c65296bb1be1d040cf8f46d3d21
sagioto <sagioto@gmail.com>
2013-02-06 14:38:36 +0200

    fixed script so that if parameters are empty string it will still work

--- centos_auto_setup_machine.sh
+++ centos_auto_setup_machine.sh
@@ -1,4 +1,3 @@
-
 #! /bin/bash
 
 
@@ -49,14 +48,14 @@ yum  -y install git
 cd play-2.0.4
 echo "cloning cloudify-widget"
 CHECKOUT_FOLDER=cloudify-widget
-if [ -z $GIT_LOCATION ] || [ "${$GIT_LOCATION+xxx}" = "xxx" ]; then
+if [ -z $GIT_LOCATION ] || [ $GIT_LOCATION"xxx" = "xxx" ]; then
     GIT_LOCATION="https://github.com/CloudifySource/cloudify-widget.git"
 fi
 echo "using git location : ${GIT_LOCATION}"
 
 git clone $GIT_LOCATION $CHECKOUT_FOLDER
 
-if [ -z $GIT_BRANCH  ] || [ "${$GIT_BRANCH+xxx}" = "xxx" ]; then
+if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
         echo "no branch specified"
 else
     echo "checking out branch ${GIT_BRANCH}"
