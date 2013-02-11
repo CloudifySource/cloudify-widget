c41ad07a50206064e0f0ecc29b40292aeba03041
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2013-02-11 10:24:35 +0200

    #50 - fixing auto-generated demo page

--- public/cloudify/js/demo.js
+++ public/cloudify/js/demo.js
@@ -1,9 +1,22 @@
 $(function(){
+        var userId = $("body").attr("data-user-id");
         jsRoutes.controllers.DemosController.listWidgetForDemoUser( $("body").attr("data-user-id")).ajax({
             success:function( result ){
                         console.log([result]);
-                        $("body").append($("widget_nav").tmpl(result)).append($("#widgets_list").tmpl(result));
+                        $("body").append($("#widget_nav").tmpl( {"widgets":result})).append($("#widgets_list").tmpl({"widgets":result, "userId":userId}));
+
+                        var $firstButton = $("button[data-api-key]:first");
+                        console.log([ $firstButton ]);
+                        $firstButton.click();
                     },
             error:function( result ){ console.log([result])}
         });
+
+    $("button[data-api-key]").live("click", function(){
+        var $this = $(this);
+        var apiKey = $this.attr("data-api-key");
+        $(".recipe").hide();
+        $(".recipe[data-api-key=" + apiKey + "]").show();
+    });
+
 });
