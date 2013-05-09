005b1462644cdaeb675a3bff313d536b8eb3ee85
Guy Mograbi <guym@gigaspaces.com>
2013-05-09 15:40:13 +0300

    removing takipi until we upgrade.

--- play_start.sh
+++ play_start.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
-if [ -f /var/lib/takipi/so/libTakipiAgent.so ]; then
-    echo "adding takipi to java options"
-    export _JAVA_OPTIONS="${_JAVA_OPTIONS} -agentpath:/var/lib/takipi/so/libTakipiAgent.so"
-fi
+#if [ -f /var/lib/takipi/so/libTakipiAgent.so ]; then
+#    echo "adding takipi to java options"
+#    export _JAVA_OPTIONS="${_JAVA_OPTIONS} -agentpath:/var/lib/takipi/so/libTakipiAgent.so"
+#fi
 nohup play -Dconfig.file=conf/prod.conf start &
