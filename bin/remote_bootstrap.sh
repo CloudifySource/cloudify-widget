d92da36f9a2178aab67e72b1a0a4bc5911a851e6
Guy Mograbi <guym@gigaspaces.com>
2013-05-12 11:22:45 +0300

    adding override to ui context xml on cloud bootstrap.

--- bin/remote_bootstrap.sh
+++ bin/remote_bootstrap.sh
@@ -3,4 +3,7 @@ CLOUD_NAME=$1
 
 cd $CLOUDIFY_HOME/bin
 
+mkdir cloudify-folder/clouds/$CLOUD_NAME/upload/cloudify-overrides/config/
+\cp -f cloudify-folder/config/cloudify-webui-context-override.xml cloudify-folder/clouds/$CLOUD_NAME/upload/cloudify-overrides/config/
+
 ./cloudify.sh "bootstrap-cloud --verbose $CLOUD_NAME"
