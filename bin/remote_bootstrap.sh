97e7b77568d7c95b47669d09449c081153dbd20f
Guy Mograbi <guym@gigaspaces.com>
2013-05-13 14:54:16 +0300

    fixing reference to cloudify folder.

--- bin/remote_bootstrap.sh
+++ bin/remote_bootstrap.sh
@@ -4,7 +4,7 @@ CLOUD_NAME=$1
 
 
 mkdir $CLOUDIFY_HOME/clouds/$CLOUD_NAME/upload/cloudify-overrides/config/
-\cp -f $CLOUDIFY_HOME/config/cloudify-webui-context-override.xml cloudify-folder/clouds/$CLOUD_NAME/upload/cloudify-overrides/config/
+\cp -f $CLOUDIFY_HOME/config/cloudify-webui-context-override.xml $CLOUDIFY_HOME/clouds/$CLOUD_NAME/upload/cloudify-overrides/config/
 
 cd $CLOUDIFY_HOME/bin
 ./cloudify.sh "bootstrap-cloud --verbose $CLOUD_NAME"
