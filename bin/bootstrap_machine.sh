4ea0c52c14ccee0b26daf584c29814ec5672ae03
guym <guym@gigaspaces.com>
2013-12-17 12:59:25 +0200

    fixing bootstrap script

--- bin/bootstrap_machine.sh
+++ bin/bootstrap_machine.sh
@@ -19,16 +19,15 @@ echo Setting sudo privileged mode
 sudo sed -i 's/^Defaults.*requiretty/#&/g' /etc/sudoers
 
 
+# http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5993-M8/gigaspaces-cloudify-2.7.0-m8-b5993.zip
 # http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5985-M3/gigaspaces-cloudify-2.7.0-M3-b5985.zip
-CLOUDIFY_VERSION=2.7.0
-BUILD_NUMBER=5985
-MILESTONE=m3
-BUILD_TYPE=SNAPSHOT
-CLOUDIFY_FOLDER=~/gigaspaces-cloudify-${CLOUDIFY_VERSION}-${MILESTONE}
+
+CLOUDIFY_URL="http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5993-M8/gigaspaces-cloudify-2.7.0-m8-b5993.zip"
+CLOUDIFY_FOLDER=~/gigaspaces-cloudify-2.7.0-m8
+
 JAVA_64_URL="http://repository.cloudifysource.org/com/oracle/java/1.6.0_32/jdk-6u32-linux-x64.bin"
 
-MILESTONE_UPPERCASE=`echo $MILESTONE | tr '[:lower:]' '[:upper:]'`
-CLOUDIFY_URL="http://repository.cloudifysource.org/org/cloudifysource/${CLOUDIFY_VERSION}-${BUILD_NUMBER}-${MILESTONE_UPPERCASE}/gigaspaces-cloudify-${CLOUDIFY_VERSION}-${MILESTONE}-b${BUILD_NUMBER}.zip"
+
 
 echo Downloading JDK from $JAVA_64_URL
 wget -q -O ~/java.bin $JAVA_64_URL
