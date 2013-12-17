a1303a787998732ef5c2eb1642009944b4fd96fc
guym <guym@gigaspaces.com>
2013-12-17 15:53:13 +0200

    more timeout.

--- bin/deployer.sh
+++ bin/deployer.sh
@@ -7,7 +7,7 @@ INSTALL_NAME=$4
 echo "ignore: CLOUDIFY_HOME is ${CLOUDIFY_HOME}"
 cd $CLOUDIFY_HOME/bin
 
-./cloudify.sh "connect http://$SERVER_IP:8100;$RECIPE_TYPE -timeout 30 $RECIPE_URL "
+./cloudify.sh "connect http://$SERVER_IP:8100;$RECIPE_TYPE -timeout 3000 $RECIPE_URL "
 #./cloudify.sh "connect http://$SERVER_IP:8100;$RECIPE_TYPE -name $INSTALL_NAME $RECIPE_URL"
 
 exit 1
