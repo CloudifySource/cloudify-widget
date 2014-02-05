9b040550815a67fe0118221f16c1f99d1124eb56
guym <guym@gigaspaces.com>
2014-02-05 13:13:06 +0200

    adding support for modules_home.

--- play_run.sh
+++ play_run.sh
@@ -5,9 +5,23 @@ if [ -z "$PLAY_HOME" ]; then
     exit 1
 fi
 
-cd modules
-mvn install
-cd ..
+if [ -z "$MODULES_HOME" ]; then
+    echo "MODULES_HOME is undefined. assuming it is at next to PLAY_HOME"
+    MODULES_HOME=`pwd`
+    MODULES_HOME=`dirname $MODULES_HOME`/cloudify-widget-modules
+    echo "settings modules to $MODULES_HOME"
+fi
+
+
+
+if [ -f $MODULES_HOME/pom.xml ]; then
+
+
+    mvn install -f $MODULES_HOME/pom.xml
+else
+    echo "pom.xml was not found under $MODULES_HOME"
+    exit 1;
+fi
 
 #this file helps run play with personal configuration
 #the me.conf file should have as first line:
