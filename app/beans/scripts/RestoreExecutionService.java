eb72918f210825780935debaeda0634d6d2a6dd3
guym <guym@gigaspaces.com>
2014-02-14 09:14:46 +0200

    adding prints on "restore executions" flow.

--- app/beans/scripts/RestoreExecutionService.java
+++ app/beans/scripts/RestoreExecutionService.java
@@ -1,7 +1,5 @@
 package beans.scripts;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -13,28 +11,29 @@ import org.slf4j.LoggerFactory;
 import play.libs.Akka;
 import server.ApplicationContext;
 import akka.util.Duration;
+import utils.CollectionUtils;
 
 public class RestoreExecutionService implements ScriptExecutorsConstants {
-	
-	private static org.slf4j.Logger logger = LoggerFactory.getLogger( RestoreExecutionService.class );
-	
-	
+
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(RestoreExecutionService.class);
+
+
     public static void restoreExecutions() {
-    	
-    	if( logger.isDebugEnabled() ){
-    		logger.debug( "---START restoreExecutions" );
-    	}
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("---START restoreExecutions");
+        }
 
 //    	restoreServerNodesBeforeBootstrap();
-    	
-    	restoreBootstrappingServerNodes();
-    	
+
+        restoreBootstrappingServerNodes();
+
 //    	restoreServerNodesAfterBootstrapAndInstallNotStarted();
 //    	restoreServerNodesAfterInstalling();    			
-	}
+    }
 
     /*
-	private static void restoreServerNodesAfterInstalling() {
+    private static void restoreServerNodesAfterInstalling() {
 		//TODO
 		File executingFolder = new File( EXECUTING_SCRIPTS_FOLDER_PATH );
 		if( executingFolder.exists() ){
@@ -82,32 +81,29 @@ public class RestoreExecutionService implements ScriptExecutorsConstants {
 		}
 	}*/
 /*
-	private static void restoreServerNodesAfterBootstrapAndInstallNotStarted() {
+    private static void restoreServerNodesAfterBootstrapAndInstallNotStarted() {
 
 		//TODO here in java code there is nothing to do, 
 		//since nodejs will move these scripts and will run bootstrap/install
 	}*/
 
-	private static void restoreBootstrappingServerNodes() {
+    private static void restoreBootstrappingServerNodes() {
 
-    	List<ServerNode> serverNodesBootstrapping = ServerNode.findByCriteria( 
-    			new ServerNode.QueryConf().setMaxRows( -1 ).
-    				criteria().
-    				setRemote( true ).
-    				setWidgetIsNull( false ).
-    				setWidgetInstanceIsNull( true ).
-    				done() );
+        List<ServerNode> serverNodesBootstrapping = ServerNode.findByCriteria(
+                new ServerNode.QueryConf().setMaxRows(-1).
+                        criteria().
+                        setRemote(true).
+                        setWidgetIsNull(false).
+                        setWidgetInstanceIsNull(true).
+
+                        done());
+        logger.info("found [{}] servers to restore", CollectionUtils.size(serverNodesBootstrapping));
+
+        if (CollectionUtils.isEmpty(serverNodesBootstrapping)) {
+            logger.info("nothing to restore");
+            return;
+        }
     	
-    	if( serverNodesBootstrapping == null ){
-    		serverNodesBootstrapping = new ArrayList<ServerNode>( 0 );
-    	}
-    	
-    	if( logger.isDebugEnabled() ){
-    		logger.debug( "---restoreBootstrappingServerNodes, " +
-    				"serverNodesBootstrapping size=" + serverNodesBootstrapping.size() );
-    		logger.debug( "---restoreBootstrappingServerNodes=" + 
-    				Arrays.toString( serverNodesBootstrapping.toArray( 
-    						new ServerNode[ serverNodesBootstrapping.size() ] ) ) );
     		/*
     		List<Widget> allWidgets = Widget.find.all();
     		logger.debug( "---All widgets=" + ( ( allWidgets == null ) ? "NULL" : 
@@ -116,47 +112,48 @@ public class RestoreExecutionService implements ScriptExecutorsConstants {
     		logger.debug( "---allWidgetInstances=" + ( allWidgetInstances == null ? "NULL" :
     			Arrays.toString( allWidgetInstances.toArray( new ServerNode[ allWidgetInstances.size() ] ) ) ) );
     			*/
-    	}
-    	for( final ServerNode serverNode : serverNodesBootstrapping ){
-        	if( logger.isDebugEnabled() ){
-        		logger.debug( "--restoreBootstrappingServerNodes, Bootstrapping serverNode ID=" + 
-        						serverNode.getId() + ", serverNode=" + serverNode );
-        	}
-        	
-        	final String serverNodeId = String.valueOf( serverNode.getId() );
-        	boolean bootstrappingFinished = 
-        			ScriptFilesUtilities.isBootstrappingFinished( serverNodeId, serverNodeId );
-        	if( !bootstrappingFinished ){
+        for (final ServerNode serverNode : serverNodesBootstrapping) {
+            restoreServerNode(serverNode);
+        }
+    }
 
-            	if( logger.isDebugEnabled() ){
-            		logger.debug( "--restoreBootstrappingServerNodes, before submitting server node to threads pool:" + serverNodeId );
-            	}
-            	
-        		//run waiting for bootstrap finishing in different thread
-                Akka.system().scheduler().scheduleOnce( 
-                	Duration.create(0, TimeUnit.SECONDS), new Runnable(){
-            		@Override
-        			public void run(){
-            			if( logger.isDebugEnabled() ){
-                    		logger.debug( "--Start running within by thread ( restore booostrapping ), serverNodeId:" + serverNodeId );
-                    	}
-            			ScriptFilesUtilities.waitForFinishBootstrappingAndSaveServerNode( serverNode, null );
-            			Widget widget = serverNode.getWidget();
-                    	if( logger.isDebugEnabled() ){
-                    		logger.debug( "--restoreExecutions, Bootstrapping" +
-                    						", serverNode ID=" + serverNodeId + 
-                    						", widget=" + widget + 
-                    						", serverNode=" + serverNode );
-                    	}
-                    	if( widget != null ){
-                    		//install application
-                    		ApplicationContext.get().getWidgetServer().deploy( widget, serverNode, null );	
-                    	}
-            		}
-            	} );
-        	}
-    	}		
-	}
+    private static void restoreServerNode(final ServerNode serverNode) {
+        logger.info("restoring execution on serverNode ID=[{}]", serverNode, serverNode.getId());
+
+        String serverNodeId = String.valueOf(serverNode.getId());
+        boolean bootstrappingFinished = ScriptFilesUtilities.isBootstrappingFinished(serverNodeId, serverNodeId);
+        if (!bootstrappingFinished) {
+            logger.info("bootstrap did not finish on nodeId [{}]", serverNode.getId());
+
+            //run waiting for bootstrap finishing in different thread
+            Akka.system().scheduler().scheduleOnce(
+                    Duration.create(0, TimeUnit.SECONDS), new ResumeExecutionTask(serverNode));
+        }
+    }
+
+    public static class ResumeExecutionTask implements Runnable {
+
+        private ServerNode serverNode;
+
+        public ResumeExecutionTask(ServerNode serverNode) {
+            this.serverNode = serverNode;
+        }
+
+        @Override
+        public void run() {
+            logger.info("waiting for bootstrap on [{}]", serverNode.getId());
+            ScriptFilesUtilities.waitForFinishBootstrappingAndSaveServerNode(serverNode, null);
+            Widget widget = serverNode.getWidget();
+            logger.info("bootstrap finished on [{}], installing widget [{}] ", serverNode.getId(), widget.getProductName());
+            if (widget != null) {
+                //install application
+                ApplicationContext.get().getWidgetServer().deploy(widget, serverNode, null);
+            } else {
+                logger.info("widget is null on [{}]. deleting serverNode", serverNode.getId());
+                serverNode.delete();
+            }
+        }
+    }
 /*
 	private static void restoreServerNodesBeforeBootstrap() {
 
