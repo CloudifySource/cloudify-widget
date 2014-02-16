7071de33130a03ff1714eeed1849c535cf9b44b0
guym <guym@gigaspaces.com>
2014-02-16 10:27:11 +0200

    implementing install scripts.

--- setup/centos.sh
+++ setup/centos.sh
@@ -7,6 +7,8 @@
 export SETUP_WORKDIR=`pwd`
 echo "My workdir is $SETUP_WORKDIR"
 
+SYSCONF_FILE=widgetSysconfig
+
 echo "checking if sysconfig file exists"
 if [ ! -f ${SYSCONF_FILE} ]; then
     echo "missing ${SYSCONF_FILE}"
@@ -15,29 +17,61 @@ fi
 
 cp /etc/sysconfig/cloudifyWidget
 
+. /etc/sysconfig/cloudifyWidget
+
+if [ -z "$1" ]; then
+    echo "usage centos.sh [ibm|hp]"
+    exit 1
+fi
+
+TYPE=$1
+
+echo "installing widget for $TYPE"
+
 echo "installing git"
 yum  -y install git
 
 mkdir -p $WIDGET_HOME
 
 echo "cloning cloudify-widget"
-CHECKOUT_FOLDER=$WIDGET_HOME
+
 if [ -z $GIT_LOCATION ] || [ $GIT_LOCATION"xxx" = "xxx" ]; then
     echo "please define a github repository to checkout from"
     exit 1;
 fi
 echo "using git location : ${GIT_LOCATION}"
 
-git clone $GIT_LOCATION $CHECKOUT_FOLDER
+if [ ! -f $WIDGET_HOME ]; then
 
-if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
-        echo "no branch specified"
+    if [ -f $WIDGET_HOME/.widgetGitLocation ]; then
+        LAST_GIT_LOCATION=`cat $WIDGET_HOME/.widgetGitLocation`
+
+        if [ "$LAST_GIT_LOCATION" != "$GIT_LOCATION" ]; then
+            echo "Last git location does not match"
+            exit 1
+        fi
+    fi
+
+    echo "cloning git repository from $GIT_LOCATION"
+    git clone $GIT_LOCATION $WIDGET_HOME
+
+    if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
+            echo "no branch specified"
+    else
+        echo "checking out branch ${GIT_BRANCH}"
+        cd $WIDGET_HOME
+        git checkout $GIT_BRANCH
+        cd -
+    fi
+
+    echo $GIT_LOCATION > ${WIDGET_HOME}/.widgetGitLocation
+
 else
-    echo "checking out branch ${GIT_BRANCH}"
-    cd $CHECKOUT_FOLDER
-    git checkout $GIT_BRANCH
-    cd -
+    echo "already checked out"
 fi
 
 
-$UTILS_FOLDER=
+export UTILS_FOLDER=${WIDGET_HOME}/setup;
+
+source $UTILS_FOLDER/${TYPE}
+
--- setup/centos_ibm.sh
+++ setup/centos_ibm.sh
@@ -1,11 +1,24 @@
 
+execute(){
+    echo "executing $1"
+    ${WIDGET_HOME}/setup/utils/$1
+}
+
+echo "clone cloudify-modules"
+
+execute install_java.sh
+
+execute install_mysql.sh
+
+execute install_nginx.sh
 
 
-echo "install git"
-echo "checkout cloudify-widget"
-echo "checkout cloudify-modules"
-echo "install java"
-echo "install mysql"
-echo "install nginx"
-echo "install ruby"
-echo "install cloudify"
+execute install_ruby.sh
+
+execute install_cloudify.sh
+
+execute install_play.sh
+
+execute install_maven.sh
+
+execute install_
