d684a31441edf4ad6597c37afecd6849de9fe87c
guym <guym@gigaspaces.com>
2014-02-16 10:36:12 +0200

    implementing install scripts.

--- setup/centos.sh
+++ setup/centos.sh
@@ -15,7 +15,7 @@ if [ ! -f ${SYSCONF_FILE} ]; then
     exit 1
 fi
 
-cp /etc/sysconfig/cloudifyWidget
+cp $SYSCONF_FILE /etc/sysconfig/cloudifyWidget
 
 . /etc/sysconfig/cloudifyWidget
 
@@ -35,40 +35,7 @@ mkdir -p $WIDGET_HOME
 
 echo "cloning cloudify-widget"
 
-if [ -z $GIT_LOCATION ] || [ $GIT_LOCATION"xxx" = "xxx" ]; then
-    echo "please define a github repository to checkout from"
-    exit 1;
-fi
-echo "using git location : ${GIT_LOCATION}"
-
-if [ ! -f $WIDGET_HOME ]; then
-
-    if [ -f $WIDGET_HOME/.widgetGitLocation ]; then
-        LAST_GIT_LOCATION=`cat $WIDGET_HOME/.widgetGitLocation`
-
-        if [ "$LAST_GIT_LOCATION" != "$GIT_LOCATION" ]; then
-            echo "Last git location does not match"
-            exit 1
-        fi
-    fi
-
-    echo "cloning git repository from $GIT_LOCATION"
-    git clone $GIT_LOCATION $WIDGET_HOME
-
-    if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
-            echo "no branch specified"
-    else
-        echo "checking out branch ${GIT_BRANCH}"
-        cd $WIDGET_HOME
-        git checkout $GIT_BRANCH
-        cd -
-    fi
-
-    echo $GIT_LOCATION > ${WIDGET_HOME}/.widgetGitLocation
-
-else
-    echo "already checked out"
-fi
+${WIDGET_HOME}/setup/utils/checkout_project.sh
 
 
 export UTILS_FOLDER=${WIDGET_HOME}/setup;
--- setup/utils/checkout_project.sh
+++ setup/utils/checkout_project.sh
@@ -1,17 +1,34 @@
-echo "cloning cloudify-widget"
-CHECKOUT_FOLDER=cloudify-widget
 if [ -z $GIT_LOCATION ] || [ $GIT_LOCATION"xxx" = "xxx" ]; then
-    GIT_LOCATION="https://github.com/CloudifySource/cloudify-widget.git"
+    echo "please define a github repository to checkout from"
+    exit 1;
 fi
 echo "using git location : ${GIT_LOCATION}"
 
-git clone $GIT_LOCATION $CHECKOUT_FOLDER
+if [ ! -f $WIDGET_HOME ]; then
 
-if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
-        echo "no branch specified"
+    if [ -f $WIDGET_HOME/.widgetGitLocation ]; then
+        LAST_GIT_LOCATION=`cat $WIDGET_HOME/.widgetGitLocation`
+
+        if [ "$LAST_GIT_LOCATION" != "$GIT_LOCATION" ]; then
+            echo "Last git location does not match"
+            exit 1
+        fi
+    fi
+
+    echo "cloning git repository from $GIT_LOCATION"
+    git clone $GIT_LOCATION $WIDGET_HOME
+
+    if [ -z $GIT_BRANCH  ] || [ $GIT_BRANCH"xxx" = "xxx" ]; then
+            echo "no branch specified"
+    else
+        echo "checking out branch ${GIT_BRANCH}"
+        cd $WIDGET_HOME
+        git checkout $GIT_BRANCH
+        cd -
+    fi
+
+    echo $GIT_LOCATION > ${WIDGET_HOME}/.widgetGitLocation
+
 else
-    echo "checking out branch ${GIT_BRANCH}"
-    cd $CHECKOUT_FOLDER
-    git checkout $GIT_BRANCH
-    cd -
+    echo "already checked out"
 fi
