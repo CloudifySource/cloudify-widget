5c00cdee98b82581fc22db90ddb0a4bb6cba439a
guym <guym@gigaspaces.com>
2014-02-17 20:26:53 +0200

    fixing install modules.

--- setup/utils/update_error_pages.sh
+++ setup/utils/update_error_pages.sh
@@ -1,4 +1,5 @@
 # assume with are in "cloudify-widget" folder
 echo "copying error pages"
 # copy content from public error_pages to that path
+mkdir -p  /var/www/cloudifyWidget/public
 \cp -Rvf ${WIDGET_HOME}/public/error_pages /var/www/cloudifyWidget/public
