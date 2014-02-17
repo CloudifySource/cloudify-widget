83ef82b7ec369247fbccd32e249071d79b136f62
guym <guym@gigaspaces.com>
2014-02-17 20:54:04 +0200

    moving nohup to be under /var/log

--- play_start.sh
+++ play_start.sh
@@ -4,4 +4,4 @@
 #    echo "adding takipi to java options"
 #    export _JAVA_OPTIONS="${_JAVA_OPTIONS} -agentpath:/var/lib/takipi/so/libTakipiAgent.so"
 #fi
-nohup play -Dconfig.file=conf/dev/me.conf start &
+nohup play -Dconfig.file=conf/dev/me.conf start &> /var/log/cloudifyWidget/nohup.out
