edfc58238665976879f1fc34e6cb1101ad7d7ef1
guym <guym@gigaspaces.com>
2014-02-18 00:31:16 +0200

    moving nohup to be under /var/log

--- setup/utils/update_db_schema.sh
+++ setup/utils/update_db_schema.sh
@@ -1,6 +1,14 @@
 echo "upgrading DB schema"
 #find which is the latest version of DB
 # ll all the files, remove "create" script, remove extension, sort in descending order and output first line.
+
+CURRENT_DIRECTORY=`pwd`
+
+cd "$(dirname "$0")"
+
 source load_sysconfig.sh
 db_version=`ls ${WIDGET_HOME}/conf/evolutions/default -1 | grep -v create |  sed -e 's/\.[a-zA-Z]*$//' | sort -r -n | head -1`
 bash ${WIDGET_HOME}/bin/migrate_db.sh $db_version
+
+
+cd $CURRENT_DIRECTORY
