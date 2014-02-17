63fee89b729aa7d93067db9e228654215567f535
guym <guym@gigaspaces.com>
2014-02-17 23:39:24 +0200

    moving nohup to be under /var/log

---
+++ setup/utils/update_me_conf.sh
@@ -0,0 +1,10 @@
+
+source load_sysconfig.sh
+
+if [ -z $ME_CONF_URL ] || [ "$ME_CONF_URL" = "" ]; then
+    echo "ME_CONF_URL not set. I will not update me conf"
+else
+    echo "downloading me.conf from [$ME_CONF_URL]"
+    mkdir -p ${WIDGET_HOME}/conf/dev
+    wget $ME_CONF_URL -O ${WIDGET_HOME}/conf/dev/me.conf
+fi
