1907b84e0898d8ed809b792a3e89aa556332db9a
guym <guym@gigaspaces.com>
2014-02-17 20:14:44 +0200

    fixing install modules.

--- setup/utils/update_db_schema.sh
+++ setup/utils/update_db_schema.sh
@@ -1,5 +1,6 @@
 echo "upgrading DB schema"
 #find which is the latest version of DB
 # ll all the files, remove "create" script, remove extension, sort in descending order and output first line.
+source load_sysconfig.sh
 db_version=`ls ${WIDGET_HOME}/conf/evolutions/default -1 | grep -v create |  sed -e 's/\.[a-zA-Z]*$//' | sort -r -n | head -1`
-${WIDGET_HOME}/bin/migrate_db.sh $db_version
+call ${WIDGET_HOME}/bin/migrate_db.sh $db_version
