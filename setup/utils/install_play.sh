7fac95e5a97a37f215505b21c4dfe874a5833ce4
guym <guym@gigaspaces.com>
2014-02-17 20:31:21 +0200

    fixing install modules.

--- setup/utils/install_play.sh
+++ setup/utils/install_play.sh
@@ -1,9 +1,12 @@
 echo "downloading play"
+
+
+
 if [ -f /usr/bin/play ]; then
     echo "play already exists - nothing to do"
 else
     wget 'http://download.playframework.org/releases/play-2.0.4.zip'  -O /tmp/play.zip
-    unzip /tmp/play.zip -d /usr/lib
+    unzip -o /tmp/play.zip -d /usr/lib
     ln -Tfs ${PLAY_HOME} /usr/lib/play
     rm -f /tmp/play.zip
 fi
