b10d5bb0c1d99319d57f5ffdb16f7c500408635a
guym <guym@gigaspaces.com>
2014-02-18 00:38:00 +0200

    moving nohup to be under /var/log

--- setup/utils/install_monit.sh
+++ setup/utils/install_monit.sh
@@ -1,6 +1,15 @@
+CURRENT_DIRECTORY=`pwd`
+
+cd "$(dirname "$0")"
+
 echo "intalling monit"
+
+source load_sysconfig.sh
+
 \cp -Rf ${WIDGET_HOME}/conf/monit/repo  /etc/yum.repos.d/epel.repo
 yum clean all
 yum -y install monit
 chkconfig --levels 235 monit on
+
+cd $CURRENT_DIRECTORY
 
