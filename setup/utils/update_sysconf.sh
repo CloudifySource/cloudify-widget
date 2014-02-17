e32c6db6b6e9eeb4c795317c7da9a69a7ca87299
guym <guym@gigaspaces.com>
2014-02-17 23:40:39 +0200

    moving nohup to be under /var/log

---
+++ setup/utils/update_sysconf.sh
@@ -0,0 +1,9 @@
+
+source load_sysconfig.sh
+
+if [ -z $SYSCONF_URL ] || [ "$SYSCONF_URL" = "" ]; then
+    echo "ME_CONF_URL not set. I will not update me conf"
+else
+    echo "downloading me.conf from [$SYSCONF_URL]"
+    wget $SYSCONF_URL -O /etc/sysconfig/cloudifyWidget
+fi
