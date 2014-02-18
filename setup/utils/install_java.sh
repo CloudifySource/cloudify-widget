3e21f7f4c1832c0898af7b884ac6c2e3b5366a62
guym <guym@gigaspaces.com>
2014-02-18 03:19:30 +0200

    fixing install_java

--- setup/utils/install_java.sh
+++ setup/utils/install_java.sh
@@ -1,6 +1,29 @@
 echo "installing java"
-yum  -y install java-1.6.0-openjdk-devel
-export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk.x86_64
-echo "export JAVA_HOME"
+
+CURRENT_DIRECTORY=`pwd`
+
+cd "$(dirname "$0")"
+
+INSTALL_JAVA_DIR=/usr/lib/jvm
+mkdir -p $INSTALL_JAVA_DIR
+cd $INSTALL_JAVA_DIR
+wget -O jdk.bin --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin
+echo "yes" | ./jdk.bin &>/dev/null
+JAVA_HOME=`pwd`/jdk1.6.0_33
+echo "JAVA_HOME is $JAVA_HOME"
+rm -f jdk.bin
 ln -Tfs $JAVA_HOME/bin/java /usr/bin/java
- ln -Tfs $JAVA_HOME/bin/javac /usr/bin/javac
+ln -Tfs $JAVA_HOME/bin/javac /usr/bin/javac
+
+# commenting out old code.. it seems that guava has bugs on certain java versions. we need to control the version.
+
+#yum  -y install java-1.6.0-openjdk-devel
+#export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk.x86_64
+#echo "export JAVA_HOME"
+#ln -Tfs $JAVA_HOME/bin/java /usr/bin/java
+# ln -Tfs $JAVA_HOME/bin/javac /usr/bin/javac
+
+
+
+
+cd $CURRENT_DIRECTORY
