aa1a08c7a408d8815d0205c976fa7e782c7511bd
guym <guym@gigaspaces.com>
2014-02-18 15:57:41 +0200

    adding mkdir on logs folder.

--- setup/utils/update_crontab.sh
+++ setup/utils/update_crontab.sh
@@ -6,10 +6,14 @@ source load_sysconfig.sh
 
 if [ -z "$CRON_EMAIL" ]; then
     echo "no cron email set.. emails will not be sent from cron"
+else
+    echo "adding MAILTO=$CRON_EMAIL"
+    echo "MAILTO=$CRON_EMAIL" > /tmp/cronconf
 fi
 
-cp ${WIDGET_HOME}/conf/cron/cron.conf /tmp/cronconf
+`cat ${WIDGET_HOME}/conf/cron/cron.conf >> /tmp/cronconf`
 
 crontab ${WIDGET_HOME}/conf/cron/cron.conf
+
 
 cd $CURRENT_DIRECTORY
