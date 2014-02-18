511dfd6a11cbce36cc8ca39f966e73e0ee826f94
guym <guym@gigaspaces.com>
2014-02-18 03:33:23 +0200

    fixing install_java

--- setup/utils/install_java.sh
+++ setup/utils/install_java.sh
@@ -8,8 +8,9 @@ INSTALL_JAVA_DIR=/usr/lib/jvm
 mkdir -p $INSTALL_JAVA_DIR
 cd $INSTALL_JAVA_DIR
 wget -O jdk.bin --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin
+chmod 755 jdk.bin
 echo "yes" | ./jdk.bin &>/dev/null
-export JAVA_HOME=`pwd`/jdk1.6.0_33
+export JAVA_HOME=/usr/lib/jvm/jdk1.6.0_33
 echo "JAVA_HOME is $JAVA_HOME"
 rm -f jdk.bin
 ln -Tfs $JAVA_HOME/bin/java /usr/bin/java
