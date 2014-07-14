96502a66587b05881756a96868125d27e4c6a01f
guym <guym@gigaspaces.com>
2014-07-14 19:56:08 +0300

    fixing install_java as oracle changed their links.

--- setup/utils/install_java.sh
+++ setup/utils/install_java.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 INSTALL_JAVA_DIR=/usr/lib/jvm
 mkdir -p $INSTALL_JAVA_DIR
 cd $INSTALL_JAVA_DIR
-wget -O jdk.bin --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin
+wget -O jdk.bin --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin
 chmod 755 jdk.bin
 echo "yes" | ./jdk.bin &>/dev/null
 export JAVA_HOME=/usr/lib/jvm/jdk1.6.0_33
