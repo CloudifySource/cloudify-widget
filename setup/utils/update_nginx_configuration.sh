3c9ab2d1a8c6ede0ef45826b765a189bb78ff649
guym <guym@gigaspaces.com>
2014-08-19 12:10:18 +0300

    removing deletion of temp site conf for debug purposes.

--- setup/utils/update_nginx_configuration.sh
+++ setup/utils/update_nginx_configuration.sh
@@ -22,6 +22,7 @@ fi
 
 cat ${WIDGET_HOME}/conf/nginx/site.nginx  | sed 's/__domain_name__/'"$SITE_DOMAIN"'/' | sed 's/__staging_name__/'"$SITE_STAGING_DOMAIN"'/' > ${TMP_SITE_CONF}
 cmp  -s ${TMP_SITE_CONF}  ${SITE_CONF_TARGET}
+echo "testing if $TMP_SITE_CONF different from $SITE_CONF_TARGET"
 if [ $? -eq 1 ]; then
     \cp -f ${TMP_SITE_CONF} ${SITE_CONF_TARGET}
     echo "restarting nginx"
