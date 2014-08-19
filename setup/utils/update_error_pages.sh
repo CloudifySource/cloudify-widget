d56473e794ec7179dffb6ff348d41b754a1f6d20
guym <guym@gigaspaces.com>
2014-08-19 11:59:14 +0300

    updating error pages upgrade.

--- setup/utils/update_error_pages.sh
+++ setup/utils/update_error_pages.sh
@@ -2,4 +2,4 @@
 echo "copying error pages"
 # copy content from public error_pages to that path
 mkdir -p  /var/www/cloudifyWidget/public
-\cp -Rvf ${WIDGET_HOME}/public/error_pages /var/www/cloudifyWidget/public
+\cp -Rvf ${WIDGET_HOME}/public-folder/error_pages /var/www/cloudifyWidget/public
